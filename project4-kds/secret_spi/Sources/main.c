6f1d8fd97c5539c188facdefd27dc84bd2386dd5
James Pentz <jpentz1@gmail.com>
2016-04-26 21:31:25 -0600

    water.... Need water..

--- project4-kds/secret_spi/Sources/main.c
+++ project4-kds/secret_spi/Sources/main.c
@@ -14,21 +14,14 @@ int main(void)
 
 	config_stuff = NORDIC_PWR_UP | NORDIC_PRIM_RX;
 
-	//spi_send_byte(ch);
-	//spi_receive_byte(&ch);
-
     for (;;) {
     	if(i == 1000000){
     		write_config(config_stuff);
     		config_stuff = 0x00;
     		read_config(&config_stuff);
-    		/*spi_receive_byte(&ch);
-    		ch = 0xaa;
-			spi_send_byte(ch);
-			spi_receive_byte(&ch);*/
     	}
          i++;
     }
-    /* Never leave main */
+
     return 0;
 }
